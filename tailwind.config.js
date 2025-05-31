b7c49a4e2b875a4b0d23f52beedeff25ae03f7a4
yujin <yujin09219@naver.com>
2025-06-01 01:57:53 +0900

    refact: 트럭 투표 페이지 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        truckMove: {
+          '0%': {
+            transform: 'translateX(-100px)',
+          },
+          '100%': {
+            transform: 'translateX(100px)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
